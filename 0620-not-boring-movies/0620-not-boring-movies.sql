4a068fd1bc90009266eed13641e29ea0d8a52348
Sravan_T <129060926+sravan-11-git@users.noreply.github.com>
2024-03-01 18:48:59 +0530

    Time: 539 ms (27.54%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select *from cinema where Not description = "boring" and id % 2 = 1 order by rating DESC;
+select *from cinema where Not description = "boring" and mod(id, 2) = 1 order by rating DESC;
